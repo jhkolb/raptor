d84e73b97dc34055750bf75a3f2aa9ba406652f2
Jack Kolb <jkolb@berkeley.edu>
2016-07-21 15:09:37 -0700

    Very rough sample config file, still debugging parser

--- frontend/build.sbt
+++ frontend/build.sbt
@@ -5,3 +5,4 @@ version := "1.0"
 scalaVersion := "2.11.8"
 
 libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.4"
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.6" % "test"
--- frontend/src/main/scala/edu/berkeley/cs/sdb/ConfigParser.scala
+++ frontend/src/main/scala/edu/berkeley/cs/sdb/ConfigParser.scala
@@ -3,11 +3,11 @@ package edu.berkeley.cs.sdb
 import scala.util.parsing.combinator._
 
 class ConfigParser extends JavaTokenParsers {
-  def path: Parser[String] = """/?([a-zA-z\._\-]+/)*[a-zA-z\._\-]+""".r
+  def path: Parser[String] = """/?([~a-zA-Z\._\-]+/)*[a-zA-Z\._\-]+""".r
   def entity = "entity" ~> path
 
 
-  def URI: Parser[String] = """([a-zA-z\._\-]/)*[a-zA-Z\._\-](/\* | /\+)?""".r
+  def URI: Parser[String] = """([a-zA-Z\._\-]+/)*[a-zA-Z\._\-]+(/\* | /\+)?""".r
   def spawnpointList: Parser[Seq[String]] = "spawnpoints" ~> "{" ~> rep1sep(URI, ",") <~ "}"
 
 
@@ -17,7 +17,7 @@ class ConfigParser extends JavaTokenParsers {
 
   def valueSequence: Parser[Seq[Any]] = "[" ~> rep1sep(value, ",") <~ "]"
 
-  def parameter: Parser[(String, Any)] = ident ~ ":" ~ (ident | value | valueSequence) ^^ { case k ~ ":" ~ v => (k,v) }
+  def parameter: Parser[(String, Any)] = ident ~ (":" | "=") ~ (ident | value | valueSequence) ^^ { case k ~ (":"|"=") ~ v => (k,v) }
 
   def parameterSequence: Parser[Seq[(String, Any)]] = "{" ~> repsep(parameter, ",") <~ "}"
 
@@ -26,9 +26,9 @@ class ConfigParser extends JavaTokenParsers {
       "where" ~> parameterSequence ^^ (Right(_))
 
   def serviceDeployment: Parser[Service] = "deploy" ~ path ~ "as" ~ ident ~ "with" ~ parameterSequence ~ spawnpointSpec ^^
-    { case "deploy" ~ imgName ~ "as" ~ svcName ~ "with" ~ params ~ spec => Service(svcName, imgName, params, spec) }
+      { case "deploy" ~ imgName ~ "as" ~ svcName ~ "with" ~ params ~ spec => Service(svcName, imgName, params, spec) }
 
-  def svcGraph: Parser[Seq[(String, String)]] = repsep(ident, "->") ^^ (_.sliding(2).map { case Seq(x, y) => (x, y) }.toSeq)
+  def svcGraph: Parser[Seq[(String, String)]] = repsep(ident, "->") ^^ (_.sliding(2).map { case List(x, y) => (x, y) }.toSeq)
 
   def deployment: Parser[Deployment] = entity ~ spawnpointList ~ rep(serviceDeployment) ~ rep(svcGraph) ^^
       { case ent ~ spawnpoints ~ svcs ~ connList => Deployment(ent, spawnpoints, svcs, connList.flatten) }
